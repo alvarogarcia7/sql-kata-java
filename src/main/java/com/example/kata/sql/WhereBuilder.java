06303644b9027eb533a6ada73310c2398d1f905a
alvaro garcia <derecerca@gmail.com>
2015-06-02 08:28:37 +0300

    refactor: save all received parameters into fields for later use

--- src/main/java/com/example/kata/sql/WhereBuilder.java
+++ src/main/java/com/example/kata/sql/WhereBuilder.java
@@ -1,11 +1,16 @@
 package com.example.kata.sql;
 
 public class WhereBuilder {
+	private String constant;
+	private String operation;
+	private WhereBuilder otherClause;
+
 	public static WhereBuilder aNew () {
 		return new WhereBuilder();
 	}
 
 	public WhereBuilder constant (final String value) {
+		this.constant = value;
 		return this;
 	}
 
@@ -14,6 +19,8 @@ public class WhereBuilder {
 	}
 
 	public WhereBuilder equalTo (final WhereBuilder where) {
+		this.operation = "=";
+		this.otherClause = where;
 		return this;
 	}
 }
