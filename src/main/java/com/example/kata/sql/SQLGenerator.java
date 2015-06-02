f23d3f70b2dba654700ef2efa065585b4118aa8e
alvaro garcia <derecerca@gmail.com>
2015-06-02 07:59:37 +0300

    refactor: remove technical debt regarding being able to select from two columns only

--- src/main/java/com/example/kata/sql/SQLGenerator.java
+++ src/main/java/com/example/kata/sql/SQLGenerator.java
@@ -1,14 +1,27 @@
 package com.example.kata.sql;
 
+import com.sun.deploy.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SQLGenerator {
 	private final String SELECT = "select ";
 	private final String FROM_CLAUSE = " from ";
 
 	public String select (final String[] columns, final String table) {
 		if(columns.length == 2) {
-			return SELECT +"a, d"+ FROM_CLAUSE + table;
+			return SELECT + getColumns(columns) + FROM_CLAUSE + table;
 		}
 		return SELECT +"a"+ FROM_CLAUSE + table;
+	}
+
+	private String getColumns (final String[] columns) {
+		final List<String> colum = new ArrayList<>();
+		for (String s : columns) {
+			colum.add(s);
+		}
+		return StringUtils.join(colum, ", ");
 	}
 
 	public String selectAll (final String table) {
