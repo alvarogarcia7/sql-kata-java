3ba4cca15d8668104c81a26720f6a358d6c2afa4
alvaro garcia <derecerca@gmail.com>
2015-06-02 08:04:06 +0300

    refactor: make constants private and final

--- src/main/java/com/example/kata/sql/SQLGenerator.java
+++ src/main/java/com/example/kata/sql/SQLGenerator.java
@@ -5,10 +5,10 @@ import com.sun.deploy.util.StringUtils;
 import java.util.Arrays;
 
 public class SQLGenerator {
-	public static final String ALL_COLUMNS = "*";
-	public static final String COLUMN_SEPARATOR = ", ";
-	private final String SELECT = "select ";
-	private final String FROM_CLAUSE = " from ";
+	private static final String ALL_COLUMNS = "*";
+	private static final String COLUMN_SEPARATOR = ", ";
+	private static final String SELECT = "select ";
+	private static final String FROM_CLAUSE = " from ";
 
 	public String select (final String[] columns, final String table) {
 		return SELECT + getColumns(columns) + FROM_CLAUSE + table;
