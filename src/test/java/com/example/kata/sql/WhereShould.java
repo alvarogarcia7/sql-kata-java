711ae93bf94e326d5d90bd370005ee560095a1da
alvaro garcia <derecerca@gmail.com>
2015-06-02 08:27:06 +0300

    refactor: static import and create helper method to reduce noise

--- src/test/java/com/example/kata/sql/WhereShould.java
+++ src/test/java/com/example/kata/sql/WhereShould.java
@@ -2,6 +2,7 @@ package com.example.kata.sql;
 
 import org.junit.Test;
 
+import static com.example.kata.sql.WhereBuilder.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -10,8 +11,12 @@ public class WhereShould {
 	@Test
 	public void equal_to_strings_none_of_them_a_column () {
 
-		assertThat(WhereBuilder.aNew().constant("a").equalTo(WhereBuilder.aNew().constant("a")).build(), is("where 'a'" +
+		assertThat(constant("a").equalTo(constant("a")).build(), is("where 'a'" +
 				" = 'a'"));
+	}
+
+	private WhereBuilder constant (final String a) {
+		return aNew().constant(a);
 	}
 
 }
